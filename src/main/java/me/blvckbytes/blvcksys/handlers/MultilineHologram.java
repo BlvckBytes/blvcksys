4c8f976c67f0d0facc7adc63d16173d6d68b99a0
Philip Hell <blvckbytes@gmail.com>
2022-05-09 02:07:05 +0200

    multilinehologram: implemented functionality

--- src/main/java/me/blvckbytes/blvcksys/handlers/MultilineHologram.java
+++ src/main/java/me/blvckbytes/blvcksys/handlers/MultilineHologram.java
@@ -1,12 +1,13 @@
 package me.blvckbytes.blvcksys.handlers;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import me.blvckbytes.blvcksys.packets.communicators.hologram.IHologramCommunicator;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
-import java.util.List;
+import java.util.*;
 
 /*
   Author: BlvckBytes <blvckbytes@gmail.com>
@@ -17,24 +18,74 @@ import java.util.List;
   variables in sync.
  */
 @Getter
-@Setter
-@AllArgsConstructor
 public class MultilineHologram {
 
   // Specify the spacing between two lines on the y-axis here
   private static final double INTER_LINE_SPACING = 0.25D;
 
-  private String name;
+  // Specify the max. squared distance between the hologram and any
+  // given recipient that receives updates here
+  private static final double RECIPIENT_MAX_DIST_SQ = Math.pow(30, 2);
+
+  private final Map<Player, List<Entity>> entities;
+  private final IHologramCommunicator holoComm;
+  private final String name;
+
   private Location loc;
   private List<String> lines;
-  private IHologramCommunicator holoComm;
+
+  public MultilineHologram(
+    String name,
+    Location loc,
+    List<String> lines,
+    IHologramCommunicator holoComm
+  ) {
+    this.name = name;
+    this.loc = loc;
+    this.lines = lines;
+    this.holoComm = holoComm;
+
+    this.entities = new HashMap<>();
+  }
+
+  //=========================================================================//
+  //                                   API                                   //
+  //=========================================================================//
+
+  public void setLines(List<String> lines) {
+    this.lines = lines;
+
+    for (Player p : Bukkit.getOnlinePlayers()) {
+      destroyLineEntities(p);
+      createLineEntities(p);
+    }
+  }
+
+  public void setLoc(Location loc) {
+    this.loc = loc;
+
+    for (Player p : Bukkit.getOnlinePlayers()) {
+      destroyLineEntities(p);
+      createLineEntities(p);
+    }
+  }
 
   /**
    * Called whenever there's a chance to update this hologram, which
    * doesn't mean that on every tick changes have to occur.
    */
   public void tick() {
-    System.out.println("Ticking hologram " + name + " with " + lines.size() + " lines");
+    for (Player t : Bukkit.getOnlinePlayers()) {
+      if (isRecipient(t))
+        tickPlayer(t);
+
+      // Don't keep players which are out of reach in memory
+      else
+        destroyLineEntities(t);
+    }
+
+    // Don't keep offline players in memory
+    cleanupOfflinePlayers();
   }
 
   /**
@@ -42,6 +93,89 @@ public class MultilineHologram {
    * all online players.
    */
   public void destroy() {
-    System.out.println("Destroying hologram " + name + " with " + lines.size() + " lines");
+    for (Player t : entities.keySet()) {
+      for (Entity ent : entities.get(t))
+        holoComm.deleteLine(t, ent);
+    }
+    entities.clear();
+  }
+
+  //=========================================================================//
+  //                                Utilities                                //
+  //=========================================================================//
+
+  /**
+   * Cleans out offline player entries from the entity map
+   */
+  private void cleanupOfflinePlayers() {
+    entities.keySet().removeIf(p -> !p.isOnline());
+  }
+
+  /**
+   * Called whenever the hologram should update for a specific player
+   * @param p Target player
+   */
+  private void tickPlayer(Player p) {
+    // Make sure that the line entities exist for this player
+    if (!this.entities.containsKey(p))
+      createLineEntities(p);
+
+    // Update all lines for this player
+    List<Entity> pEnts = entities.get(p);
+    for (int i = 0; i < Math.min(pEnts.size(), lines.size()); i++) {
+      String lineText = lines.get(i);
+      Entity ent = pEnts.get(i);
+      holoComm.updateLine(p, ent, lineText);
+    }
+  }
+
+  /**
+   * Create all existing line entities based on the
+   * current state for a player
+   * @param p Target player
+   */
+  private void createLineEntities(Player p) {
+    List<Entity> ents = new ArrayList<>();
+
+    // Make lines grow downwards from the head
+    Location head = loc.clone();
+    for (String line : lines) {
+      ents.add(holoComm.createLine(p, head, line));
+      head.add(0, -INTER_LINE_SPACING, 0);
+    }
+
+    entities.put(p, ents);
+  }
+
+  /**
+   * Destroy all existing line entities for a player
+   * @param p Target player
+   */
+  private void destroyLineEntities(Player p) {
+    List<Entity> ents = entities.remove(p);
+
+    // Had no lines
+    if (ents == null)
+      return;
+
+    // Destroy all lines
+    for (Entity ent : ents)
+      holoComm.deleteLine(p, ent);
+  }
+
+  /**
+   * Checks if the given player is a recipient of this hologram
+   * @param p Player to test for
+   * @return True if should receive updates, false otherwise
+   */
+  private boolean isRecipient(Player p) {
+    Location pLoc = p.getLocation();
+
+    // Not in the same world
+    if (loc.getWorld() != pLoc.getWorld())
+      return false;
+
+    // Check if the player is within reach
+    return loc.distanceSquared(pLoc) <= RECIPIENT_MAX_DIST_SQ;
   }
 }

a7ea13bad55e18ef1d15f11b3d40b8382c1fcc94
Philip Hell <blvckbytes@gmail.com>
2022-05-21 02:57:04 +0200

    deathlistener: implemented automatic respawn

---
+++ src/main/java/me/blvckbytes/blvcksys/events/DeathListener.java
@@ -0,0 +1,64 @@
+package me.blvckbytes.blvcksys.events;
+
+import me.blvckbytes.blvcksys.di.AutoConstruct;
+import me.blvckbytes.blvcksys.di.AutoInject;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+  Author: BlvckBytes <blvckbytes@gmail.com>
+  Created On: 05/21/2022
+
+  Listens to death events and automatically respawns the player after
+  the respawn delay elapsed. Does the same on joins if the player is dead.
+*/
+@AutoConstruct
+public class DeathListener implements Listener {
+
+  // Delay in ticks after death till the automatic respawn
+  private final static long RESPAWN_DELAY_T = 15;
+
+  private final JavaPlugin plugin;
+
+  public DeathListener(
+    @AutoInject JavaPlugin plugin
+  ) {
+    this.plugin = plugin;
+  }
+
+  //=========================================================================//
+  //                                  Listener                               //
+  //=========================================================================//
+
+  @EventHandler
+  public void onDeath(PlayerDeathEvent e) {
+    respawnPlayer(e.getEntity());
+  }
+
+  @EventHandler
+  public void onJoin(PlayerJoinEvent e) {
+    if (!e.getPlayer().isDead())
+      return;
+
+    respawnPlayer(e.getPlayer());
+  }
+
+  //=========================================================================//
+  //                                 Utilities                               //
+  //=========================================================================//
+
+  /**
+   * Respawn the player after the delay elapsed
+   * @param p Target player
+   */
+  private void respawnPlayer(Player p) {
+    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+      p.spigot().respawn();
+    }, RESPAWN_DELAY_T);
+  }
+}

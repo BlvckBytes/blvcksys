ea29ade708d350a0d486fae25eaaac1c4e89d0bd
Philip Hell <blvckbytes@gmail.com>
2022-05-26 04:06:13 +0200

    damagelistener: implemented kill indicators

--- src/main/java/me/blvckbytes/blvcksys/events/PlayerDamageListener.java
+++ src/main/java/me/blvckbytes/blvcksys/events/PlayerDamageListener.java
@@ -6,15 +6,18 @@ import me.blvckbytes.blvcksys.di.AutoConstruct;
 import me.blvckbytes.blvcksys.di.AutoInject;
 import me.blvckbytes.blvcksys.handlers.IHologramHandler;
 import me.blvckbytes.blvcksys.handlers.MultilineHologram;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
+import java.util.List;
 import java.util.Random;
 
 /*
@@ -22,7 +25,8 @@ import java.util.Random;
   Created On: 05/26/2022
 
   Listens to damage done by a player to another player and spawns a temporary
-  hologram which displays information about the damage done.
+  hologram which displays information about the damage done. Also spawns kill
+  indicators when a player has been killed by a hit.
 */
 @AutoConstruct
 public class PlayerDamageListener implements Listener {
@@ -39,18 +43,27 @@ public class PlayerDamageListener implements Listener {
   // Number of hologram vectors to prepare beforehand for the ringbuffer
   private static final int NUM_VECTORS = 20;
 
+  // Time in ticks to display the upwards floating kill indicator for
+  private static final long KILL_INDICATOR_DUR_T = 33;
+
+  // Y velocity of the kill indicator
+  private static final double KILL_INDICATOR_YVEL = 0.1;
+
   private final IHologramHandler holos;
   private final IConfig cfg;
+  private final JavaPlugin plugin;
 
   private final Vector[] hologramVectors;
   private int hologramVectorsInd;
 
   public PlayerDamageListener(
     @AutoInject IHologramHandler holos,
-    @AutoInject IConfig cfg
+    @AutoInject IConfig cfg,
+    @AutoInject JavaPlugin plugin
   ) {
     this.holos = holos;
     this.cfg = cfg;
+    this.plugin = plugin;
 
     this.hologramVectors = new Vector[NUM_VECTORS];
 
@@ -70,12 +83,34 @@ public class PlayerDamageListener implements Listener {
     if (!(e.getDamager() instanceof Player damager))
       return;
 
+    if (victim.getHealth() - e.getDamage() <= 0)
+      spawnKillIndicator(victim, damager);
+
     spawnDamageIndicator(victim, damager, e.getDamage());
   }
 
   //=========================================================================//
   //                                 Utilities                               //
   //=========================================================================//
+
+  /**
+   * Spawn a new kill indicating temporary hologram
+   * @param victim Player that has been killed
+   * @param killer Player that has killed
+   */
+  private void spawnKillIndicator(Player victim, Player killer) {
+    MultilineHologram holo = holos.createTemporary(
+      victim.getLocation(),
+      List.of(killer),
+      cfg.get(ConfigKey.KILL_INDICATORS)
+        .withVariable("victim", victim.getName())
+        .withVariable("coins", 10)
+        .asList()
+    );
+
+    holo.setVelocity(new Vector(0, KILL_INDICATOR_YVEL, 0), 0D, false, false, null, null);
+    Bukkit.getScheduler().runTaskLater(plugin, () -> holos.destroyTemporary(holo), KILL_INDICATOR_DUR_T);
+  }
 
   /**
    * Spawn a new damage indicating temporary hologram
@@ -85,12 +120,12 @@ public class PlayerDamageListener implements Listener {
    */
   private void spawnDamageIndicator(Player victim, Player damager, double damage) {
     MultilineHologram holo = holos.createTemporary(
-      victim.getLocation().add(0, LOCATION_YOFF, 0),
+      victim.getLocation().add(0, LOCATION_YOFF, 0), null,
       cfg.get(isCriticallyDamaging(damager) ? ConfigKey.DAMAGE_INDICATORS_CRITICAL : ConfigKey.DAMAGE_INDICATORS_NORMAL)
-        .withVariable("damage", Math.round(damage * 100) / 100)
+        .withVariable("damage", Math.round(damage * 100.0) / 100.0)
         .asList()
     );
-    holo.setVelocity(getNextVector(), () -> holos.destroyTemporary(holo));
+    holo.setVelocity(getNextVector(), null, true, true, null, () -> holos.destroyTemporary(holo));
   }
 
   /**

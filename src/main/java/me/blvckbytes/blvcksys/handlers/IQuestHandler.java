485969eb0616d66fa52e0a3d7800a096828a92f7
Philip Hell <blvckbytes@gmail.com>
2022-06-25 21:40:03 +0200

    quests: started implementing the quest handler

---
+++ src/main/java/me/blvckbytes/blvcksys/handlers/IQuestHandler.java
@@ -0,0 +1,9 @@
+package me.blvckbytes.blvcksys.handlers;
+
+/*
+  Author: BlvckBytes <blvckbytes@gmail.com>
+  Created On: 06/25/2022
+
+  Public interfaces which the quest handler provides to other consumers.
+*/
+public interface IQuestHandler {}
